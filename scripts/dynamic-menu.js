bdeef7db15ab94a7e52b17e09ed63909d6347406
tonakihan <116086641+tonakihan@users.noreply.github.com>
2023-09-07 17:48:35 +0900

    rm jquery

--- scripts/dynamic-menu.js
+++ scripts/dynamic-menu.js
@@ -1,8 +1,17 @@
-// Ожидаю готовность DOM
-$(document).ready(function() {
-    $("#target").on("change", function() {
-        $("select option:selected").each(function() {
-            alert($(this).val());
-        });
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.forms[0];
+    let select = form.elements.target;
+
+    let target = select.value;
+
+    // TECT
+    let div = document.createElement("div");
+    div.innerHTML = target;
+    form.appendChild(div); 
+    // END TECT
+
+    select.onchange = function() {
+        target = select.value;
+        div.innerHTML = target; //TECT
+    }
 });
